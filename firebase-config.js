c5330b4273df2929adb8e7a97ae6d4e99b6088de
HimanshuJihi <nshuhima246@gmail.com>
2026-02-08 12:35:41 +0530

    Add search and notification features; implement follow/unfollow functionality with user stats

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// d:\New cricket game\firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyDCeBjF5RT-VeTuYBKrqIn-ITjBhapYLyo",
+    authDomain: "autiointoarena.firebaseapp.com",
+    projectId: "autiointoarena",
+    storageBucket: "autiointoarena.firebasestorage.app",
+    messagingSenderId: "189047143221",
+    appId: "1:189047143221:web:aa35971cca4dcfa26dea7c",
+    measurementId: "G-5QCHD08LTM"
+};
